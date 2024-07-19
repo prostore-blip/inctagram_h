8f83c361d46a7aa19ecd290ef32022e5d2934a4c
Chrizzoman <sasuke123451997@gmail.com>
2024-07-19 09:45:30 +0300

    deploy

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,5 +3,5 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  return <div>Page hellodssdsd</div>
+  return <div>Page hello</div>
 }
