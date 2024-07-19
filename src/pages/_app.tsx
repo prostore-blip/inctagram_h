45f6327fe3eb5bb75fdb41477d68efe253671fb4
Chrizzoman <sasuke123451997@gmail.com>
2024-07-19 15:16:34 +0300

    Refactoring

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,7 @@ import { ReactElement, ReactNode } from 'react'
 import { NextPage } from 'next'
 
 import '../styles/index.scss'
+// eslint-disable-next-line import/extensions
 import '@chrizzo/ui-kit/css'
 import '../styles/_colors.scss'
 import '../styles/_typography.scss'
