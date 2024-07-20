2aa63aeeba0d2ba948428ded4ea6bf745f2c9e73
“evan-weissrussland” <evanwodichka@gmail.com>
2024-07-20 15:31:35 +0300

    IN-13 fix: refactor header / make adaptive designe

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,2 +1,76 @@
-import s from '@/components/header/header.module.scss'
-export const Header = () => <header className={s.header}>header</header>
+import { useEffect, useState } from 'react'
+
+import Notief from '@/assets/icons/svg/Fill bell.svg'
+import NotiefWithCount from '@/assets/icons/svg/Mask.svg'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+
+import s from './header.module.scss'
+
+const countNotifies = 1
+
+export const Header = () => {
+  const [windowWidth, setWindowWidth] = useState(0)
+
+  const router = useRouter()
+  const isAuthMe = false
+  const goToLogIn = () => {
+    router.push('/signIn')
+  }
+  const goToSignUp = () => {
+    router.push('/signUp')
+  }
+  const isNotiefShowStyle = isAuthMe ? '' : s.displaynone
+
+  const toShowNotifiesHandler = () => {}
+
+  useEffect(() => {
+    if (!windowWidth) {
+      setWindowWidth(window.innerWidth)
+    }
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth)
+    }
+
+    window.addEventListener('resize', handleResize)
+
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [])
+
+  return (
+    <div className={s.header}>
+      <div className={s.wrapper}>
+        <Link href={'/'}>Inctagram</Link>
+        <div className={s.buttonsBlock}>
+          {windowWidth > 450 ? (
+            <div className={`${s.noties} ${isNotiefShowStyle}`} onClick={toShowNotifiesHandler}>
+              {countNotifies ? (
+                <>
+                  <span className={s.countNotifies}>{countNotifies}</span> <NotiefWithCount />{' '}
+                </>
+              ) : (
+                <Notief />
+              )}
+            </div>
+          ) : (
+            <></>
+          )}
+          <div className={s.selectBlock}>select</div>
+          {!isAuthMe && windowWidth > 680 && (
+            <div className={s.buttonsContainer}>
+              <button className={s.login} onClick={goToLogIn} type={'button'}>
+                LogIn
+              </button>
+              <button className={s.signIn} onClick={goToSignUp} type={'button'}>
+                SignUp
+              </button>
+            </div>
+          )}
+          {isAuthMe && windowWidth < 769 && <div className={s.dropDown}>***</div>}
+        </div>
+      </div>
+    </div>
+  )
+}
