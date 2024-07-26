14958a3f0d6a35fcc3aec59b3d5891087ad02b1d
Chrizzoman <sasuke123451997@gmail.com>
2024-07-26 16:33:28 +0300

    socialAuthButtons

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,7 @@
 export * from './Layout'
 export * from './Layout/baseLayout'
 export * from './Layout/navLayout'
+export * from './auth'
 export * from './headMeta'
 export * from './header'
 export * from './main'
