bad3cf1b1ed4d63ce21dc31787c82a6d6a53accb
Chrizzoman <sasuke123451997@gmail.com>
2024-07-26 16:33:38 +0300

    socialAuthButtons

---
+++ src/assets/icons/googleIcon.tsx
@@ -0,0 +1,75 @@
+import { SVGProps } from 'react'
+
+export const GoogleIcon = ({ height, width, ...props }: SVGProps<SVGSVGElement>) => {
+  return (
+    <svg
+      fill={'none'}
+      height={'36.000000'}
+      viewBox={'0 0 36 36'}
+      width={'36.000000'}
+      xmlns={'http://www.w3.org/2000/svg'}
+    >
+      <desc>Created with Pixso.</desc>
+      <defs>
+        <clipPath id={'clip3663_9633'}>
+          <rect
+            fill={'white'}
+            fillOpacity={'0'}
+            height={'35.000000'}
+            id={'google-svgrepo-com 1'}
+            rx={'-0.500000'}
+            transform={'translate(0.500000 0.500000)'}
+            width={'35.000000'}
+          />
+        </clipPath>
+      </defs>
+      <rect
+        fill={'#FFFFFF'}
+        fillOpacity={'0'}
+        height={'35.000000'}
+        id={'google-svgrepo-com 1'}
+        rx={'-0.500000'}
+        transform={'translate(0.500000 0.500000)'}
+        width={'35.000000'}
+      />
+      <g clipPath={'url(#clip3663_9633)'}>
+        <path
+          d={
+            'M7.89 14.64C9.29 10.4 13.28 7.36 18 7.36C20.53 7.36 22.82 8.26 24.62 9.73L29.86 4.5C26.67 1.71 22.58 0 18 0C10.9 0 4.79 4.04 1.85 9.97L7.89 14.64Z'
+          }
+          fill={'#EA4335'}
+          fillOpacity={'1.000000'}
+          fillRule={'nonzero'}
+          id={'Vector'}
+        />
+        <path
+          d={
+            'M24.06 27.01C22.42 28.07 20.34 28.63 18 28.63C13.29 28.63 9.32 25.61 7.91 21.4L1.85 26C4.78 31.94 10.89 36 18 36C22.39 36 26.6 34.43 29.75 31.49L24.06 27.01Z'
+          }
+          fill={'#34A853'}
+          fillOpacity={'1.000000'}
+          fillRule={'nonzero'}
+          id={'Vector'}
+        />
+        <path
+          d={
+            'M29.75 31.49C33.04 28.42 35.18 23.85 35.18 18C35.18 16.93 35.01 15.79 34.77 14.72L18 14.72L18 21.68L27.65 21.68C27.17 24.02 25.89 25.83 24.06 27.01L29.75 31.49Z'
+          }
+          fill={'#4A90E2'}
+          fillOpacity={'1.000000'}
+          fillRule={'nonzero'}
+          id={'Vector'}
+        />
+        <path
+          d={
+            'M7.91 21.4C7.55 20.33 7.36 19.19 7.36 18C7.36 16.82 7.55 15.7 7.89 14.64L1.85 9.97C0.65 12.39 0 15.11 0 18C0 20.87 0.66 23.59 1.85 26L7.91 21.4Z'
+          }
+          fill={'#FBBC05'}
+          fillOpacity={'1.000000'}
+          fillRule={'nonzero'}
+          id={'Vector'}
+        />
+      </g>
+    </svg>
+  )
+}
