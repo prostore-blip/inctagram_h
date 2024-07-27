d889c13834292e56161fb629437ccd3072054dd7
“evan-weissrussland” <evanwodichka@gmail.com>
2024-07-27 19:03:52 +0300

    IN-16 fix: add createApi from RTKQ

---
+++ src/services/inctagram.service.ts
@@ -0,0 +1,9 @@
+import { baseQueryWithReauth } from '@/services/inctagram.fetch-base-query'
+import { createApi } from '@reduxjs/toolkit/query/react'
+
+export const inctagramService = createApi({
+  baseQuery: baseQueryWithReauth,
+  endpoints: builder => ({}),
+  reducerPath: 'inctagramService',
+  tagTypes: ['', ''],
+})
