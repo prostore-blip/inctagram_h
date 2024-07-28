e02171dd60f999d5a98cf2247753664439f09904
“evan-weissrussland” <evanwodichka@gmail.com>
2024-07-28 21:44:55 +0300

    IN-16 feat: add i18n

---
+++ src/locales/en.ts
@@ -0,0 +1,20 @@
+import { LocaleType } from '@/locales/ru'
+
+export const en: LocaleType = {
+  header: {
+    signInButton: 'Sign In',
+    signUpButton: 'Sign Up',
+  },
+  publicPage: {
+    title: 'Public Page',
+  },
+  signIn: {
+    dontHaveAcc: 'Don&apos;t have an account?',
+    emailTitle: 'Email',
+    forgotPass: 'Forgot Password',
+    passTitle: 'Password',
+    signInButton: 'SignIn',
+    signUp: 'SignUp',
+    title: 'SignIn',
+  },
+}
