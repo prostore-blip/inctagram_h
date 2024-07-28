f5da78172ede167dfbf4f615f3567d87a7eb8c6a
“evan-weissrussland” <evanwodichka@gmail.com>
2024-07-28 18:55:39 +0300

    IN-16 feat: add protected routes Layout: LoginNavigate / wrap private pages in to this Layout

---
+++ src/hoc/LoginNavigate.tsx
@@ -0,0 +1,18 @@
+import { FC, PropsWithChildren } from 'react'
+
+import { useAuthMeQuery } from '@/services/inctagram.auth.service'
+import { useRouter } from 'next/router'
+
+export const LoginNavigate: FC<PropsWithChildren<{}>> = ({ children }) => {
+  const router = useRouter()
+  const { data, isLoading } = useAuthMeQuery()
+
+  if (isLoading) {
+    return <div style={{ fontSize: '50px' }}>...Loading</div>
+  }
+  if (!data) {
+    void router.push('/signIn')
+  }
+
+  return <>{children}</>
+}
