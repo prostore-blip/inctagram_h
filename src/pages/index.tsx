f7fab88ca38bd634fecbd3ed2adf90bf82fdaa5d
“evan-weissrussland” <evanwodichka@gmail.com>
2024-07-28 18:53:49 +0300

    IN-16 fix: change start page to PublicPage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,19 @@
-import { GetNavLayout, PageWrapper } from '@/components'
+import { GetLayout, PageWrapper } from '@/components'
+import { LoginNavigate } from '@/hoc/LoginNavigate'
 
-export default function Home() {
-  return (
-    <>
-      <PageWrapper>Home Page</PageWrapper>
-    </>
-  )
+// export default function Home1() {
+//   return (
+//     <>
+//       <LoginNavigate>
+//         <PageWrapper>Home Page</PageWrapper>
+//       </LoginNavigate>
+//     </>
+//   )
+// }
+// Home1.getLayout = GetLayout
+export function PublicPage() {
+  return <PageWrapper>PublicPage</PageWrapper>
 }
-Home.getLayout = GetNavLayout
+
+PublicPage.getLayout = GetLayout
+export default PublicPage
