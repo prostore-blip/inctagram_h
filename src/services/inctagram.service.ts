55f084dc0adcda8eed4d785826d1623bb0971417
“evan-weissrussland” <evanwodichka@gmail.com>
2024-07-28 18:52:09 +0300

    IN-16 fix: refactor servise RTKQ

--- src/services/inctagram.service.ts
+++ src/services/inctagram.service.ts
@@ -1,8 +1,17 @@
-import { baseQueryWithReauth } from '@/services/inctagram.fetch-base-query'
+import { fetchBaseQuery } from '@reduxjs/toolkit/query'
 import { createApi } from '@reduxjs/toolkit/query/react'
 
 export const inctagramService = createApi({
-  baseQuery: baseQueryWithReauth,
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://inctagram.work/api',
+    prepareHeaders: headers => {
+      const token = localStorage.getItem('token')
+
+      headers.set('Authorization', `Bearer ${token}`)
+
+      return headers
+    },
+  }),
   endpoints: builder => ({}),
   reducerPath: 'inctagramService',
   tagTypes: ['', ''],
