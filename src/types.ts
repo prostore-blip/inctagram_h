b8771913995f968c93b09378d26cbaa194481ca0
Aleksandr Savkin <voyager5874@gmail.com>
2024-08-06 18:29:27 +0300

    feat: create type guard for submitted form errors

--- src/types.ts
+++ src/types.ts
@@ -3,3 +3,31 @@ export type ErrorData = {
   messages: { field: string; message: string }[]
   statusCode: number
 }
+
+export type FormError = {
+  data: ErrorData
+}
+
+export function isFormErrorData(obj: unknown): obj is ErrorData {
+  return (
+    typeof obj === 'object' &&
+    obj !== null &&
+    'statusCode' in obj &&
+    typeof obj.statusCode === 'number' &&
+    'messages' in obj &&
+    Array.isArray(obj.messages) &&
+    // obj.messages.every(
+    //   (msg: any) =>
+    //     typeof msg === 'object' &&
+    //     msg !== null &&
+    //     typeof msg.message === 'string' &&
+    //     typeof msg.field === 'string'
+    // ) &&
+    'error' in obj &&
+    typeof obj.error === 'string'
+  )
+}
+
+export function isFormError(obj: unknown): obj is FormError {
+  return typeof obj === 'object' && obj !== null && 'data' in obj && isFormErrorData(obj.data)
+}
