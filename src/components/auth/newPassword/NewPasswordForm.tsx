c51d607b3032754476b0561d2d202d10d9f131e1
Aleksandr Savkin <voyager5874@gmail.com>
2024-08-08 11:30:48 +0300

    refactor: change validation mode

--- src/components/auth/newPassword/NewPasswordForm.tsx
+++ src/components/auth/newPassword/NewPasswordForm.tsx
@@ -35,6 +35,7 @@ export const NewPasswordForm = () => {
       newPassword: '',
       recoveryCode: recoveryCode,
     },
+    mode: 'onBlur',
     resolver: zodResolver(newPasswordSchema),
   })
 
