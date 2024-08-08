7cd9d3d60c27f6d042c96693269cee8c8fad29ab
Aleksandr Savkin <voyager5874@gmail.com>
2024-08-08 11:30:17 +0300

    feat: add regexp for password strength validation to the schema

--- src/components/auth/newPassword/schema.ts
+++ src/components/auth/newPassword/schema.ts
@@ -1,10 +1,20 @@
 import { z } from 'zod'
 
 //todo apply the same regexp as for sign-up
+//todo translate validation errors somehow
 export const newPasswordSchema = z
   .object({
-    confirmPassword: z.string().min(6, 'Password has to be at least 6 characters long'),
-    newPassword: z.string().min(6, 'Password has to be at least 6 characters long'),
+    confirmPassword: z.string().min(6, 'Passwords do not match'),
+    newPassword: z
+      .string()
+      .min(6, 'Password has to be at least 6 characters long')
+      .regex(/[0-9]/, 'Password must contain at least one digit')
+      .regex(/[a-z]/, 'Password must contain at least one lowercase letter')
+      .regex(/[A-Z]/, 'Password must contain at least one uppercase letter')
+      .regex(
+        /[!"#$%&'()*+,-./:;<=>?@[\\\]^_`{|}~]/,
+        'Password must contain at least one special character'
+      ),
     recoveryCode: z.string(),
   })
   .refine(data => data.newPassword === data.confirmPassword, {
