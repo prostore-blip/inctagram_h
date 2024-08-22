38d50bf31925178cc186d10b092b8e530c4fc28f
“evan-weissrussland” <evanwodichka@gmail.com>
2024-08-22 21:53:20 +0300

    fix log-in: delete message for email input

--- src/components/auth/logIn/logIn-schema.tsx
+++ src/components/auth/logIn/logIn-schema.tsx
@@ -1,11 +1,7 @@
 import { z } from 'zod'
 
 export const logInSchema = z.object({
-  email: z.string({ message: 'Поля не должны быть пустыми' }).email({
-    message:
-      'Password must contain 0-9, a-z, A-Z, ! " # $ % &\n' +
-      "' ( ) * + , - . / : ; < = > ? @ [ \\ ] ^ _` { | } ~",
-  }),
+  email: z.string({ message: 'Поля не должны быть пустыми' }).email(),
   password: z
     .string({ message: 'Поля не должны быть пустыми' })
     .min(3, { message: 'Minimum number of characters 6; Maximum number of characters 20' }),
