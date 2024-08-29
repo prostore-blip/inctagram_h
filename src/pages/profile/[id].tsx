3445a2ce0775212f39abf9f9da00a481a31f3cf4
“evan-weissrussland” <evanwodichka@gmail.com>
2024-08-29 21:34:50 +0300

    fix dinamic public page: add SSG

--- src/pages/profile/[id].tsx
+++ src/pages/profile/[id].tsx
@@ -1,32 +1,51 @@
 import { PageWrapper } from '@/components'
 import { UserProfile } from '@/components/userProfile'
-import { useAuthMeQuery } from '@/services/inctagram.auth.service'
-import { useGetMyProfileQuery } from '@/services/inctagram.profile.service'
-import { useGetPublicProfileForUserByIdQuery } from '@/services/inctagram.public-user.service'
-import { useRouter } from 'next/router'
+import { GetServerSideProps, InferGetServerSidePropsType } from 'next'
 
 import s from './userProfilePage.module.scss'
 
-function UserProfileDinamicPage() {
-  const { data: authMe, isFetching: isFetchingAuthMe } = useAuthMeQuery()
-  const router = useRouter()
+/**
+ * SSR
+ */
+export const getServerSideProps = (async ({ params }) => {
+  /**
+   * запрос me
+   */
+  const res = await fetch('https://inctagram.work/api/v1/auth/me')
+  const me: any = await res.json()
 
-  const { data: publicProfileUserData, isFetching: isFetchingPublicProfileUser } =
-    useGetPublicProfileForUserByIdQuery(Number(router.query.id), {
-      skip: isFetchingAuthMe,
-    })
+  /**
+   * если я залогинен, то делаю запрос за своим профилем, иначе за публичным
+   */
+  if (me.userId) {
+    const resProfile = await fetch(`https://inctagram.work/api/v1/users/profile`)
+    const profile: any = await resProfile.json()
 
-  const { data, isFetching } = useGetMyProfileQuery(undefined, {
-    skip: isFetchingAuthMe || !authMe,
-  })
+    return { props: { myProfileId: me.userId, profile } }
+  } else {
+    const resProfile = await fetch(
+      `https://inctagram.work/api/v1/public-user/profile/${params?.id}`
+    )
+    const profile: any = await resProfile.json()
 
+    return { props: { myProfileId: null, profile } }
+  }
+}) satisfies GetServerSideProps<{ myProfileId: null | undefined; profile: any }>
+
+/**
+ * Компонент
+ */
+function UserProfileDinamicPage(props: {
+  pageProps: InferGetServerSidePropsType<typeof getServerSideProps>
+}) {
   return (
     <PageWrapper>
       <div className={s.overflowedContainer}>
         <div className={s.mainCntainer}>
-          {!isFetching && !isFetchingAuthMe && !isFetchingPublicProfileUser && (
-            <UserProfile dataProfile={publicProfileUserData || data} myProfileId={authMe?.userId} />
-          )}
+          <UserProfile
+            dataProfile={props.pageProps.profile}
+            myProfileId={props.pageProps.myProfileId}
+          />
         </div>
       </div>
     </PageWrapper>
