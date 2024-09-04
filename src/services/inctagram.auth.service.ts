3bf141e2b243df2df94ecf83915d3723820a6b6a
“evan-weissrussland” <evanwodichka@gmail.com>
2024-09-04 21:25:38 +0300

    fix: add comment and TODO in login endpoint in authService

--- src/services/inctagram.auth.service.ts
+++ src/services/inctagram.auth.service.ts
@@ -14,6 +14,11 @@ export const inctagramAuthService = inctagramService.injectEndpoints({
           const res = await queryFulfilled
 
           localStorage.setItem('token', res.data.accessToken)
+          /**
+           * добавляем свою куку с токеном. Это нужно для выполнения запросов на защищённые эндпоинты при SSR, SSG
+           */
+          //TODO время 'expires=' в куке внутри login выбрано произвольно. Необходимо ли синхронизировать его с временем из куки, но там непонятное время - 1970год
+          document.cookie = `access_token=${res.data.accessToken}; expires=${new Date('2050-09-04').toUTCString()}; SameSite=None; Secure`
           dispatch(inctagramAuthService.util.invalidateTags(['login']))
         },
         query: body => {
