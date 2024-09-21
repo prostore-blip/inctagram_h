b0cbbcaef86fa480f9e9699f3bcbff08437c641c
“evan-weissrussland” <evanwodichka@gmail.com>
2024-09-22 00:37:46 +0300

    fix: add scroll for Privacy Policy / delete redirect to login for unautorized user

--- src/pages/privacyPolicy/index.tsx
+++ src/pages/privacyPolicy/index.tsx
@@ -1,9 +1,16 @@
 import { GetLayout, PageWrapper } from '@/components'
 import { PolicyDocuments } from '@/components/auth/policyDocuments'
+import { Scroll } from '@/components/scroll'
 
 export function PrivacyPolicy() {
-  return <PolicyDocuments headline={'Privacy Policy'} />
+  return (
+    <PageWrapper>
+      <Scroll height={'calc(100vh - 61px)'}>
+        <PolicyDocuments headline={'Privacy Policy'} />
+      </Scroll>
+    </PageWrapper>
+  )
 }
 
-PrivacyPolicy.getLayout = GetLayout
+// PrivacyPolicy.getLayout = GetLayout
 export default PrivacyPolicy
