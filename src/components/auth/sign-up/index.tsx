b7342ad1ec367b58665b394ac9794cb5382ffc4c
prostore-blip <prostore923@gmail.com>
2024-09-27 15:00:10 +0300

    fix: form validation display

--- src/components/auth/sign-up/index.tsx
+++ src/components/auth/sign-up/index.tsx
@@ -22,7 +22,7 @@ export const SingUpComponent = () => {
     control,
     formState: { errors, isDirty, isSubmitting, isValid, isValidating },
     handleSubmit,
-  } = useForm<SignUpFormType>({ resolver: zodResolver(signUpSchema) })
+  } = useForm<SignUpFormType>({ resolver: zodResolver(signUpSchema), mode: 'onTouched',})
 
   const [singUp, { data, isError, isLoading }] = useSignUpMutation()
   const { executeRecaptcha, loaded: recaptchaReady } = useReCaptcha()
