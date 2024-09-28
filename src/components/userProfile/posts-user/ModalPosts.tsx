13d06601bd3f0002fb35b752530ba5b8d31a18c6
“evan-weissrussland” <evanwodichka@gmail.com>
2024-09-28 14:59:36 +0300

    IN-61 fix: delete unused code

--- src/components/userProfile/posts-user/ModalPosts.tsx
+++ src/components/userProfile/posts-user/ModalPosts.tsx
@@ -27,19 +27,10 @@ type Props = {
 }
 
 const ModalPost = ({ post, showMore, userName }: Props) => {
-  const router = useRouter()
-  const queryParams = router.query
-
-  /**
-   * хук useState для управления open/close AlertDialog.Root. Нужна только для skip'а запроса за
-   * комментариями, если модалка не открыта. В компоненте Modalka можно не использовать
-   */
-  const [open, setOpen] = useState(false)
-
   /**
    * запрос за комментариями к посту
    */
-  const { data, isFetching } = useGetCommentsQuery()
+  const { data } = useGetCommentsQuery()
 
   if (data) {
     localStorage.removeItem('postId')
@@ -48,34 +39,14 @@ const ModalPost = ({ post, showMore, userName }: Props) => {
   /**
    * дата создания поста
    */
-  // const date = new Date(post.createdAt)
+  //TODO необходимо в new Date пробрасывать дату создания поста с сервера,
+  // но такие данные не предусмотрены бэкэндерами, поэтому хардкод
   const date = new Date('2024-09-25T11:02:57.570Z')
   const options: DateTimeFormatOptions = { day: 'numeric', month: 'long', year: 'numeric' }
   const createdPostDate = date.toLocaleDateString('en-US', options)
 
-  /**
-   * добавляем query-параметры в url. А именно id открытого поста. Это нужно, чтобы, когды мы открыли модалку
-   * поста, в url появился id этого поста. Далее мы можем скопировать url и переслать другу. Он перейдёт
-   * по нашей ссылке и у него откроется модалка поста автоматически. Без id в url при переходе по ссылку,
-   * как понять, модалку какого поста открыть, ведь подгрузитсястраница пользователя с несколькими постами?
-   * При закрытии модалки, убираем query-параметры.
-   */
-  // useEffect(() => {
-  //   if (open && !queryParams.postId) {
-  //     router.replace({
-  //       pathname: router.asPath,
-  //       query: { postId: post.id },
-  //     })
-  //   }
-  //   if (!open && Number(queryParams.postId) === post.id) {
-  //     router.replace({
-  //       pathname: `${post.ownerId}`,
-  //     })
-  //   }
-  // }, [open])
-
   return (
-    <Modal onOpenChange={setOpen} open={open}>
+    <Modal>
       <ModalTrigger asChild>
         <Image alt={'avatar'} height={228} src={defaultAva} width={234} />
       </ModalTrigger>
